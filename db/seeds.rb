89929890725411bddac228d4c4fe3516d424bc7c
rafalpilawka <rav260@wp.pl>
2015-02-27 10:22:34 +0100

    Task six

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,15 @@ user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
 
-#city_list = [
- # [ "Czestochowa", "Czwa"],
-  #[ "Moscow", "Mos"],
-  #[ "Tokyo", "Tky"],
-  #[ "Kopenhagen","Cph"]
-#]
+city_list = [
+  [ "Czestochowa", "Czwa"],
+  [ "Moscow", "Mos"],
+  [ "Tokyo", "Tky"],
+  [ "Kopenhagen","Cph"]
+			]
 
-#city_list.each do |name, short_name|
- # City.create( name: name, short_name: short_name )
-#end
+city_list.each do |name, short_name|
+  City.create( name: name, short_name: short_name )
+end
 
 
